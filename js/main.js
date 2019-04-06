e4ed4c34bba86c1ab4a36902fbcf9665eb79255c
chiney <tanakrid.c@ku.th>
2019-04-06 14:22:30 +0700

    add home page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var a = 10;
+    var b = 20;
+    var c = 30;
+    document.getElementById("demo").innerHTML = a+b+c;
+    window.alert(a+b+c);
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+// document.getElementById("demo").innerHTML = 5 + 6;
